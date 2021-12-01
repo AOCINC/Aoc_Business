ab60919aabaf5bbb85445ba40e6cb2551e3ebdd7
aocinc <aocincpvtltd@gmail.com>
2021-12-01 16:24:37 +0530

    jobs apply form changes

--- Aoc_Business/JobsApp/urls.py
+++ Aoc_Business/JobsApp/urls.py
@@ -5,5 +5,6 @@ from JobsApp import views
 urlpatterns = [
     path('Jobs-List',views.JobsListView, name ='Jobs_List'),
     path('Job-Upload',views.Job_UploadsView, name = 'Jobs_Upload'),
-    path('JobDetial/<int:id>/',views.Jobs_Details_View, name = 'JobDetail')
+    path('JobDetial/<int:id>/',views.Jobs_Details_View, name = 'JobDetail'),
+    path('Job-Apply',views.JobApply, name = 'Job_Apply'),
 ]
--- Aoc_Business/JobsApp/views.py
+++ Aoc_Business/JobsApp/views.py
@@ -58,7 +58,7 @@ def JobsListView(request):
     }
     return render(request, template, context)
 
-
+        
 
 def Jobs_Details_View(request,id=None):
     """
@@ -69,15 +69,87 @@ def Jobs_Details_View(request,id=None):
     """
     template = 'JobsApp/Jobs_Detials.html'
     JobDetail = Job_Upload.objects.get(id = id)
+    absolute_Url = request.build_absolute_uri() # getting full url to redirect if some thing went wrong in form.
+    jobId = JobDetail.id
+    request.session['jobId'] = jobId
+    # request.session['JobDetail'] = JobDetail
     if request.method == "POST":
         name        = request.POST.get('user_name')
         phone       = request.POST.get('user_phone')
         email       = request.POST.get('user_email')
         objectId    = request.POST.get('objectId')
-        JobFile     = request.FILES['JobFile']
+        if name == '' or name == None or phone == '' or phone == None or email == '' or email == None:
+            messages.warning(request,'please fill the form to submit the your request.')
+            return redirect(absolute_Url)
+        if name == '' or name == None:
+            messages.warning(request,'please enter your name to submit the form')
+            return redirect(absolute_Url)
+        if len(request.FILES) != 0:
+            JobFile     = request.FILES['JobFile']
+        else:
+            messages.warning(request, 'Please Upload Your C.V to Process Your Request.')
+            return redirect(absolute_Url)
         selected_job= Job_Upload.objects.get(id = objectId)
         designation = selected_job.Designation
-        print(JobFile,'=====')
+        # mail sending to admin of aocinc
+        subject = 'AOCINC JOB PORTAL MAIL,JOB APPLIED BY \t' + name
+        context = {
+            'name':name,
+            'phone':phone,
+            'email':email,
+            'designation':designation,
+        }
+        from_email = EMAIL_HOST_USER
+        to         = [EMAIL_HOST_USER]
+        message = get_template('JobsApp/Aocinc_Jobs_Mail.html').render(context)
+        msg = EmailMessage(subject, message, to=to,from_email = from_email)
+        msg.attach(JobFile.name, JobFile.read(), JobFile.content_type)                    # attaching a single file from the form to send it to mail as attachement.
+        msg.content_subtype  = 'html'
+        # msg.send()
+        messages.success(request,'Job Applied Successfully..')
+        # mail sending To Admin ends here.
+        # mail sending To Applied User
+        subject2 = "AOCINC JOB PORTAL MAIL,SUCCESSFULLY APPLIED"
+        context = {
+            'name':name,
+            'designation':designation,
+        }
+        from_email = EMAIL_HOST_USER
+        to         = [email]
+        message = get_template('JobsApp/Aocinc_Jobs_Mail_To_User.html').render(context)
+        msg = EmailMessage(subject2, message, to=to,from_email = from_email)
+        msg.content_subtype  = 'html'
+        # msg.send()
+        return redirect('Jobs_List')
+        # mail sending To Applied User Ends here
+    context  = {
+        'JobDetail':JobDetail,
+    }
+    return render(request,template,context)
+
+
+
+def JobApply(request):
+    template  = 'JobsApp/JobApplyForm.html'
+    absolute_Url = request.build_absolute_uri() # getting full url to redirect if some thing went wrong in form.
+    jobId = request.session['jobId']
+    JobDetail = Job_Upload.objects.get(id = jobId)
+    designation = JobDetail.Designation
+    if request.method == 'POST':
+        name        = request.POST.get('user_name')
+        phone       = request.POST.get('user_phone')
+        email       = request.POST.get('user_email')
+        if name == '' or name == None or phone == '' or phone == None or email == '' or email == None:
+            messages.warning(request,'please fill the form to submit the your request.')
+            return redirect(absolute_Url)
+        if name == '' or name == None:
+            messages.warning(request,'please enter your name to submit the form')
+            return redirect(absolute_Url)
+        if len(request.FILES) != 0:
+            JobFile     = request.FILES['JobFile']
+        else:
+            messages.warning(request, 'Please Upload Your C.V to Process Your Request.')
+            return redirect(absolute_Url)
         # mail sending to admin of aocinc
         subject = 'AOCINC JOB PORTAL MAIL,JOB APPLIED BY \t' + name
         context = {
@@ -93,7 +165,7 @@ def Jobs_Details_View(request,id=None):
         msg.attach(JobFile.name, JobFile.read(), JobFile.content_type)                    # attaching a single file from the form to send it to mail as attachement.
         msg.content_subtype  = 'html'
         msg.send()
-        messages.success(request,'Job Applied Successfully..')
+        messages.success(request,'Job Applied Successfully for -' + designation)
         # mail sending To Admin ends here.
         # mail sending To Applied User
         subject2 = "AOCINC JOB PORTAL MAIL,SUCCESSFULLY APPLIED"
@@ -109,7 +181,5 @@ def Jobs_Details_View(request,id=None):
         msg.send()
         return redirect('Jobs_List')
         # mail sending To Applied User Ends here
-    context  = {
-        'JobDetail':JobDetail,
-    }
-    return render(request,template,context)
+
+    return render(request,template)
